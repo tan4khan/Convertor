cfc47da1e382800ae01091dab382fd6b7bee1f21
Tanweer khan <tanweernkhan@gmail.com>
2023-08-16 22:15:18 +0530

    setup mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import { useState } from 'react'
 import './App.css'
 import HomePage from './Component/HomePage'
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
 
 function App() {
-  
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#1976D2",
+      },
+      secondary: {
+        main: "#FFC107",
+      },
+    },
+    // Other theme configurations...
+  });
+
 
   return (
     <>
+    <ThemeProvider theme={theme}>
      <HomePage/>
+     </ThemeProvider>
     </>
   )
 }
--- src/Component/HomePage.jsx
+++ src/Component/HomePage.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-import { Box,Typography } from '@mui/material';
+import { Box,Typography,styled } from '@mui/material';
 
 
+
+const StyledBox = styled(Box)(({ theme }) => ({
+    backgroundColor: theme.palette.primary.main,
+    color: theme.palette.secondary.main,
+    // padding: theme.spacing(2),
+    width:"100%",
+    height:"100vh",
+    borderRadius: theme.shape.borderRadius,
+  }))
 const HomePage = () => {
   return (
-  <Box>
+  <StyledBox>
 <Typography variant='h6'>Hii</Typography>
-  </Box>
+  </StyledBox>
   )
 }
 
