f97a639434333ad152616754c51316b89ecb9121
Tanweer khan <tanweernkhan@gmail.com>
2023-08-16 22:09:29 +0530

    Homepage

---
+++ src/Component/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Box,Typography } from '@mui/material';
+
+
+const HomePage = () => {
+  return (
+  <Box>
+<Typography variant='h6'>Hii</Typography>
+  </Box>
+  )
+}
+
+export default HomePage
